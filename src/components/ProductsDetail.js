ad101ae8022b9ce5a030c67b1cd087e7021b253a
TanviDobariya1 <tanvidobariya7@gmail.com>
2024-03-13 22:41:35 +0530

    fixed

--- src/components/ProductsDetail.js
+++ src/components/ProductsDetail.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ProductsDetail = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='container m-auto flex w-full flex-row gap-10 border border-blue-500 justify-center'>
+      <div className='border border-black'>
+      ferwfew
+      </div>
+      <div className='border border-black'>
+      frewrew
+      </div>
     </div>
   )
 }
